8ae3853a9a327fd1826c282713183e78d269904d
Ismoilkhon <54812099+ismoilkhon@users.noreply.github.com>
2023-01-27 18:13:12 +0500

    Implemented check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const stack = []
+  const openingBrackets = bracketsConfig.map(arr => arr[0])
+  const closingBrackets = bracketsConfig.map(arr => arr[1])
+
+  for (let char of str) {
+    if (closingBrackets.includes(char) && char === stack[stack.length - 1]) {
+      stack.pop()
+      continue
+    }
+
+    if (openingBrackets.includes(char)) {
+      stack.push(char)
+      continue
+    }
+
+    const [opening] = bracketsConfig.find(config => char === config[1])
+    
+    if (stack.pop() !== opening) {
+      return false
+    }
+  }
+
+  return stack.length === 0
 }
